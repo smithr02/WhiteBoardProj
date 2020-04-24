0838f025713af56db82a9938db9fec8ff414595b
Ryan Smith <ryan_smith2@hotmail.com>
2020-04-24 17:11:00 -0500

    Changed the code run counting backwards from 9 to the console.

--- WhiteBoardChallenges/Program.cs
+++ WhiteBoardChallenges/Program.cs
@@ -12,7 +12,7 @@ namespace WhiteBoardChallenges
         {
             
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 9; i >= 0; i--)
             {
                 Console.WriteLine(i);            
             }
